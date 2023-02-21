d8986a6cea06e9a268d607bc45cce4b3760d4c6f
rblomquist <rblomquist@gmail.com>
2023-02-21 11:20:49 -0500

    auth0 testing

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,45 @@
 const router = require("express").Router();
 const tyranids = require("./tyranids");
 const wolves = require("./wolves");
-const auth = require("./auth");
+// const auth = require("./auth");
+
+const express = require('express');
+const app = express();
+
+require("dotenv").config();
+const { auth, requiresAuth } = require('express-openid-connect');
+router.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
+  })
+);
+
+
 
 router.get('/', (req, res) => {
-    res.send('Home Page');
+    res.send('Home Page')
+    // res.send(req.oidc.isAuthenticated() ? "logged in" : "Logged out");
   });
+// router.get('/login', (req, res) =>
+//   res.oidc.login({
+//     returnTo: '/profile',
+//     authorizationParams: {
+//       redirect_uri: 'http://localhost:3000',
+//     },
+//   })
+// );
+router.get('/callback', (req, res, next) => {
+  res.redirect("https://youtube.com");
+  next()
+})
 
 router.use("/tyranids", tyranids);
 router.use("/wolves", wolves);
-router.use("/auth", auth);
+// router.use("/auth", auth);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,16 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require("./db/connect");
 
-const passport = require("passport");
-const session = require("express-session");
-require("./passport")(passport);
-
+// const passport = require("passport");
+// const session = require("express-session");
+// require("./passport")(passport);
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+// const { auth } = require('express-openid-connect');
+
+// app.use(
+//   auth({
+//     authRequired: false,
+//     auth0Logout: true,
+//     issuerBaseURL: process.env.ISSUER_BASE_URL,
+//     baseURL: process.env.BASE_URL,
+//     clientID: process.env.CLIENT_ID,
+//     secret: process.env.SECRET,
+//   })
+// );
+// app.use(auth(config));
+
 
 app
 .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
@@ -20,14 +34,15 @@ app
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 })
-.use(session({
-  secret: "warhammer",
-  resave: false,
-  saveUninitialized: false,
-}))
-.use(passport.initialize())
-.use(passport.session())
+// .use(session({
+//   secret: "warhammer",
+//   resave: false,
+//   saveUninitialized: false,
+// }))
+// .use(passport.initialize())
+// .use(passport.session())
 .use('/', require("./routes"));
+
 
 mongodb.initDb((err, mongodb) => {
     if (err) {
