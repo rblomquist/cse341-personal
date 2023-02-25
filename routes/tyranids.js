866e4e0d812230d49405be863bb87745ba2ec48a
rblomquist <rblomquist@gmail.com>
2023-02-25 14:06:20 -0500

    validation fix

--- routes/tyranids.js
+++ routes/tyranids.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const model = require("../models/tyranidsModel");
+const { validate } = require("../helper/validation-middleware");
 const { requiresAuth } = require('express-openid-connect');
 
 
 router.get("/", model.getAll);
 router.get("/:id", model.getSingle);
-router.post("/add", requiresAuth(), model.addList);
+router.post("/add", requiresAuth(), validate(), model.addList);
 router.delete("/delete/:id", requiresAuth(), model.deleteList);
-router.put("/update/:id", requiresAuth(), model.updateList);
+router.put("/update/:id", requiresAuth(), validate(), model.updateList);
 
 module.exports = router;
